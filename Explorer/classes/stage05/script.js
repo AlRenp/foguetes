10791acbcabe985e65493993169514bbc4120e6d
AlRenp <alyssonrenacc@gmail.com>
2023-08-26 22:13:09 -0300

    adicionando animação à entrada

--- Explorer/classes/stage05/script.js
+++ Explorer/classes/stage05/script.js
@@ -1,8 +1,10 @@
-let randomNum = Math.round(Math.random() * 10)
+// let randomNum = Math.round(Math.random() * 10)
+let randomNum = 1
 let aux = 1
 
 const btnTry = document.querySelector('#inp-sub');
 const btnAgain = document.querySelector('#try-again');
+const app = document.querySelector('#app')
 
 
 btnTry.addEventListener('click', handleTryClick)
@@ -14,17 +16,49 @@ function handleTryClick() {
     const screen = document.querySelector('#winner');
 
     if (Number(inputValue.value) == randomNum) {
+        winnerAnimation()
         toggleClass();
         screen.querySelector('h1').innerText = `Acertou em ${aux} tentativas`
+    } else {
+        errorAnimation()
+        inpuFocus(200)
     }
     inputValue.value = ""
     aux++
 }
 
+function inpuFocus(value) {
+    const inp = document.querySelector('#inp-num')
+
+    setTimeout(() => {
+        inp.focus()
+    }, value)
+}
+
+function winnerAnimation() {
+
+    app.classList.add('animate__flip')
+    setTimeout(() => {
+        app.classList.remove('animate__flip')
+    }, 800)
+}
+
+function errorAnimation() {
+    const tryWrapper = document.querySelector('#try-wrapper')
+
+    tryWrapper.classList.add('erro')
+
+    setTimeout(() => {
+        tryWrapper.classList.remove('erro')
+    }, 200)
+}
+
 function resetGame() {
-    toggleClass();
+    toggleClass()
     aux = 1
-    randomNum = Math.round(Math.random() * 10)
+    inpuFocus(1)
+    exitApp()
+    // randomNum = Math.round(Math.random() * 10)
 }
 
 function toggleClass() {
