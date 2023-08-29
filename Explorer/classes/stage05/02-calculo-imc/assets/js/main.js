83fb08c87ba66c2c112022c8a41f261bde7d210a
AlRenp <alyssonrenacc@gmail.com>
2023-08-29 16:22:53 -0300

    add new projetc

---
+++ Explorer/classes/stage05/02-calculo-imc/assets/js/main.js
@@ -0,0 +1,19 @@
+/*  
+    [x] Pegar elementos 'INPUTS', 'BUTTON IMC', 'BUTTON CLOSE POPUP'
+    [ ] Pegar  valor dos inputs
+    [ ] Adicionar click no Button IMC
+    [ ] Adicionar click no button CLOSE POPUP
+    
+    [ ] Validar dado do input (string = false, intero = true)
+    [ ] Verifcar tipo de dado do input
+    [ ] Aparecer com aviso de dados incorretos (Digite somente números)
+    [ ] Calcular IMC
+
+    Cliclar em calcular IMC
+    Pegar valor dos inputs
+    Verificar dados dos inputs
+    Validar dados
+    Aparecer com aviso de dados incorretos a depeder da validação
+
+*/
+
