2ebc5d8db7e5a3f2337436f089dc39e262f1f142
AlRenp <alyssonrenacc@gmail.com>
2024-02-06 22:33:27 -0300

    add new project

---
+++ Discover/challanges/switcher/assets/js/main.js
@@ -0,0 +1,10 @@
+
+const btn_mode = document.getElementById('mode')
+
+btn_mode.addEventListener('click', () => {
+
+    document.documentElement.classList.toggle('light')
+    // document.querySelector('.ph-moon-stars').classList.toggle('hide')
+    // document.querySelector('.ph-sun-dim').classList.toggle('hide')
+
+})
